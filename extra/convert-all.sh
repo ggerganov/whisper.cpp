130b5c02d6fdbf24fadde2764e8dc761cb9739d8
Georgi Gerganov <ggerganov@gmail.com>
2022-10-17 18:37:06 +0300

    Adding helper script for converting the PT models

---
+++ extra/convert-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+models=( "tiny.en" "tiny" "base.en" "base" "small.en" "small" "medium.en" "medium" "large" )
+
+for model in "${models[@]}"; do
+    python3 convert-pt-to-ggml.py ~/.cache/whisper/$model.pt ../whisper models/
+    mv -v models/ggml-model.bin models/ggml-$model.bin
+done
