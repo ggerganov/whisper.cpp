a6dbd9188b13378dc36e2c669b9a22e17b4201d1
Georgi Gerganov <ggerganov@gmail.com>
2023-01-14 19:20:47 +0200

    stream : fix a bug that inserted a lot of empty audio at the start
    The quality was terrible due to this

--- examples/stream/stream.cpp
+++ examples/stream/stream.cpp
@@ -459,7 +459,7 @@ int main(int argc, char ** argv) {
     struct whisper_context * ctx = whisper_init_from_file(params.model.c_str());
 
     std::vector<float> pcmf32    (n_samples_30s, 0.0f);
-    std::vector<float> pcmf32_old(n_samples_30s, 0.0f);
+    std::vector<float> pcmf32_old;
     std::vector<float> pcmf32_new(n_samples_30s, 0.0f);
 
     std::vector<whisper_token> prompt_tokens;
