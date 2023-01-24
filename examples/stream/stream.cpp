4bfbc65ede36499c67b0e333ac1f1fe38e3228cf
Brayden McLean <braydenmclean@gmail.com>
2023-01-23 20:43:46 -0800

    new line

--- examples/stream/stream.cpp
+++ examples/stream/stream.cpp
@@ -681,7 +681,7 @@ int main(int argc, char ** argv) {
                         if (params.fname_out.length() > 0) {
                             std::time_t now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
                             fout << "[" << std::put_time(std::localtime(&now_c), "%F %T") << "]  ";
-                            fout << stext;
+                            fout << stext << std::endl;
                         }
                     } 
                     else {
