b2fc4c701001d0b1cab1c1185e31458a290e0b32
polarmoon <90010972+polarmoon@users.noreply.github.com>
2023-02-03 23:09:27 -0800

    go : support "auto" as an option when set language (#462)
    Co-authored-by: Ming <ming@localhost>

--- bindings/go/pkg/whisper/interface.go
+++ bindings/go/pkg/whisper/interface.go
@@ -29,7 +29,7 @@ type Model interface {
 
 // Context is the speach recognition context.
 type Context interface {
-	SetLanguage(string) error // Set the language to use for speech recognition.
+	SetLanguage(string) error // Set the language to use for speech recognition, use "auto" for auto detect language.
 	SetTranslate(bool)        // Set translate flag
 	IsMultilingual() bool     // Return true if the model is multilingual.
 	Language() string         // Get language
