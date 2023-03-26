3627ef51f688793c0430f6e1591496aa1d2a6cff
Georgi Gerganov <ggerganov@gmail.com>
2023-03-26 23:54:52 +0300

    minor

--- examples/talk.llama/speak.sh
+++ examples/talk.llama/speak.sh
@@ -9,9 +9,12 @@
 #
 #espeak -v en-us+m$1 -s 225 -p 50 -a 200 -g 5 -k 5 "$2"
 
+# for Mac
+say "$2"
+
 # Eleven Labs
 #
-wd=$(dirname $0)
-script=$wd/eleven-labs.py
-python3 $script $1 "$2" >/dev/null 2>&1
-ffplay -autoexit -nodisp -loglevel quiet -hide_banner -i ./audio.mp3 >/dev/null 2>&1
+#wd=$(dirname $0)
+#script=$wd/eleven-labs.py
+#python3 $script $1 "$2" >/dev/null 2>&1
+#ffplay -autoexit -nodisp -loglevel quiet -hide_banner -i ./audio.mp3 >/dev/null 2>&1
