86ecfc633316ed04b2c9ec86f3709aa7af10469a
Lucas Zanek <57494138+LucasZNK@users.noreply.github.com>
2023-03-29 17:59:17 -0300

    whisper.addon : fixed test to new async implementation (#686)
    * fixed blocking code on node addon
    
    * modify the example to run async
    
    * format
    
    * added logic to see the whisper output
    
    * added logic to see the whisper output
    
    * removed extra function for more clean example
    
    * fixed whisper test to new async implementation

--- examples/addon.node/__test__/whisper.spec.js
+++ examples/addon.node/__test__/whisper.spec.js
@@ -1,15 +1,22 @@
-const path = require('path');
-const { whisper } = require(path.join(__dirname, '../../../build/Release/whisper-addon'));
+const path = require("path");
+const { whisper } = require(path.join(
+  __dirname,
+  "../../../build/Release/whisper-addon"
+));
+const { promisify } = require("util");
+
+const whisperAsync = promisify(whisper);
 
 const whisperParamsMock = {
-    language: 'en',
-    model: path.join(__dirname, '../../../models/ggml-base.en.bin'),
-    fname_inp: path.join(__dirname, '../../../samples/jfk.wav'),
+  language: "en",
+  model: path.join(__dirname, "../../../models/ggml-base.en.bin"),
+  fname_inp: path.join(__dirname, "../../../samples/jfk.wav"),
 };
 
 describe("Run whisper.node", () => {
+  test("it should receive a non-empty value", async () => {
+    let result = await whisperAsync(whisperParamsMock);
 
-    test("it should receive a non-empty value", () => {
-        expect(whisper(whisperParamsMock).length).toBeGreaterThan(0);
-    });
+    expect(result.length).toBeGreaterThan(0);
+  });
 });
