264d60ba3c74fa7a08e9e926d18c4ce9c55e852a
Aaron Farnham <aaron@foreflight.com>
2023-05-19 22:48:13 -0500

    Add Swift Package Manager manifest
    Add Swift Package manifest

---
+++ swiftpm/whisper-wrapper.h
@@ -0,0 +1,15 @@
+//
+//  whisper-wrapper.h
+//  
+//
+//  Created by Aaron Farnham on 5/19/23.
+//
+
+
+/*
+ Swift Package Manager recursively searchers for headers in it's
+ public headers path. Create a segregated path so only necessary
+ headers are included in the Swift Package.
+ */
+
+#include "../whisper.h"
