447d49530c9af41fe24f2ae510f452903dba330d
Georgi Gerganov <ggerganov@gmail.com>
2023-11-24 13:13:12 +0200

    whisper : remove trailing whitespaces

--- whisper.h
+++ whisper.h
@@ -51,7 +51,7 @@ extern "C" {
     //     ...
     //
     //     whisper_context_params cparams = whisper_context_default_params();
-    // 
+    //
     //     struct whisper_context * ctx = whisper_init_from_file_with_params("/path/to/ggml-base.en.bin", cparams);
     //
     //     if (whisper_full(ctx, wparams, pcmf32.data(), pcmf32.size()) != 0) {
@@ -316,7 +316,7 @@ extern "C" {
     WHISPER_API const char * whisper_lang_str(int id);
 
     // Return the short string of the specified language name (e.g. 2 -> "german"), returns nullptr if not found
-    WHISPER_API const char * whisper_lang_str_full(int id); 
+    WHISPER_API const char * whisper_lang_str_full(int id);
 
     // Use mel data at offset_ms to try and auto-detect the spoken language
     // Make sure to call whisper_pcm_to_mel() or whisper_set_mel() first
