0f5b5bea496168a5b3c722f549a306ed3e43bf34
bobqianic <129547291+bobqianic@users.noreply.github.com>
2024-02-05 11:29:08 +0000

    Update test_whisper.rb

--- bindings/ruby/tests/test_whisper.rb
+++ bindings/ruby/tests/test_whisper.rb
@@ -110,13 +110,6 @@ class TestWhisper < Test::Unit::TestCase
     assert !@params.token_timestamps
   end
 
-  def test_split_on_word
-    @params.split_on_word = true
-    assert @params.split_on_word
-    @params.split_on_word = false
-    assert !@params.split_on_word
-  end
-
   def test_speed_up
     @params.speed_up = true
     assert @params.speed_up
