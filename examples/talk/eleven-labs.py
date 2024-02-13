6584e6301bef37f1dfdb5be49664bc47b7ce2e63
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-02-13 20:21:56 +0900

    talk: sync with talk-llama
    Passing text_to_speak as a file is safer and more portable
    cf. https://stackoverflow.com/a/59036879/45375

--- examples/talk/eleven-labs.py
+++ examples/talk/eleven-labs.py
@@ -1,20 +1,34 @@
 import sys
-import importlib.util
+import argparse
 
-if importlib.util.find_spec("elevenlabs") is None:
-    print("elevenlabs library is not installed, you can install it to your enviroment using 'pip install elevenlabs'")
-    sys.exit()
+parser = argparse.ArgumentParser(description="Generate the TTS")
+parser.add_argument("inputfile")
+parser.add_argument("-v", "--voice", type=int, default=2,
+    choices=[0, 1, 2, 3, 4, 5, 6],
+    help="Get a voice object by number")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-s", "--savefile", default="audio.mp3",
+    help="Save the TTS to a file")
+group.add_argument("-p", "--play", action="store_true",
+    help="Play the TTS with ffplay")
+parser.add_argument("-q", "--quiet", action="store_true",
+    help="Quietly skip checking the required library")
+args = parser.parse_args()
+
+if not args.quiet:
+    import importlib.util
+    if importlib.util.find_spec("elevenlabs") is None:
+        print("elevenlabs library is not installed, you can install it to your enviroment using 'pip install elevenlabs'")
+        sys.exit()
 
 from elevenlabs import generate, play, save
 
-# Get a Voice object, by name or UUID
-voice = "Arnold" #Possible Voices: Adam Antoni Arnold Bella Domi Elli Josh
-
-# Generate the TTS
-audio = generate(
-  text=str(sys.argv[2:]),
-  voice=voice
-)
-
-# Save the TTS to a file
-save(audio, "audio.mp3") 
+with open(args.inputfile) as f:
+    audio = generate(
+        text=str(f.read()),
+        voice=["Adam", "Antoni", "Arnold", "Bella", "Domi", "Elli", "Josh"][args.voice]
+    )
+    if args.play:
+        play(audio)
+    else:
+        save(audio, args.savefile) 
--- examples/talk/speak.ps1
+++ examples/talk/speak.ps1
@@ -1,12 +1,14 @@
 # Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
 param(
-  # voice options are David or Zira
-  [Parameter(Mandatory=$true)][string]$voice,
-  [Parameter(Mandatory=$true)][string]$text
+  [Parameter(Mandatory=$true)][int]$voicenum,
+  [Parameter(Mandatory=$true)][string]$textfile
 )
 
 Add-Type -AssemblyName System.Speech;
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;
+$voiceoptions = "David", "Zira";
+$voice = $voiceoptions[$voicenum % $voiceoptions.count];
 $speak.SelectVoice("Microsoft $voice Desktop");
 $speak.Rate="0";
+$text = Get-Content -Path $textfile;
 $speak.Speak($text);
