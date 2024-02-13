fd8ee9084ef5b7edc1315724f74854dc0679513b
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-02-14 00:45:09 +0900

    talk and talk-llama: get all installed voices in speak.ps1

--- examples/talk-llama/speak.ps1
+++ examples/talk-llama/speak.ps1
@@ -6,9 +6,9 @@ param(
 
 Add-Type -AssemblyName System.Speech;
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;
-$voiceoptions = "David", "Zira";
+$voiceoptions = $speak.GetInstalledVoices("en-US");
 $voice = $voiceoptions[$voicenum % $voiceoptions.count];
-$speak.SelectVoice("Microsoft $voice Desktop");
+$speak.SelectVoice($voice.VoiceInfo.Name);
 $speak.Rate="0";
 $text = Get-Content -Path $textfile;
 $speak.Speak($text);
