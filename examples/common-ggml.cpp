1711bb388182a2e59c9534ec7181ea33aebd68f3
Georgi Gerganov <ggerganov@gmail.com>
2024-02-28 12:59:11 +0200

    sync : llama.cpp (ggml/0)

--- examples/common-ggml.cpp
+++ examples/common-ggml.cpp
@@ -64,10 +64,12 @@ bool ggml_common_quantize_0(
         case GGML_FTYPE_MOSTLY_Q4_1_SOME_F16:
         case GGML_FTYPE_MOSTLY_IQ2_XXS:
         case GGML_FTYPE_MOSTLY_IQ2_XS:
+        case GGML_FTYPE_MOSTLY_IQ2_S:
         case GGML_FTYPE_MOSTLY_IQ3_XXS:
         case GGML_FTYPE_MOSTLY_IQ3_S:
         case GGML_FTYPE_MOSTLY_IQ1_S:
         case GGML_FTYPE_MOSTLY_IQ4_NL:
+        case GGML_FTYPE_MOSTLY_IQ4_XS:
                 {
                     fprintf(stderr, "%s: invalid model type %d\n", __func__, ftype);
                     return false;
@@ -199,10 +201,12 @@ bool ggml_common_quantize_0(
                 case GGML_TYPE_Q8_K:
                 case GGML_TYPE_IQ2_XXS:
                 case GGML_TYPE_IQ2_XS:
+                case GGML_TYPE_IQ2_S:
                 case GGML_TYPE_IQ3_XXS:
                 case GGML_TYPE_IQ3_S:
                 case GGML_TYPE_IQ1_S:
                 case GGML_TYPE_IQ4_NL:
+                case GGML_TYPE_IQ4_XS:
                 case GGML_TYPE_COUNT:
                     {
                         fprintf(stderr, "%s: unsupported quantization type %d (%s)\n", __func__, ttype, ggml_type_name((ggml_type) ttype));
