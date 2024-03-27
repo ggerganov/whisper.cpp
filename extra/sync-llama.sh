37acf5e42e7a17b731c27bbf72fc307c65ad352a
Georgi Gerganov <ggerganov@gmail.com>
2024-03-27 13:32:53 +0200

    sync : update scripts

--- extra/sync-llama.sh
+++ extra/sync-llama.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp -rpv ../llama.cpp/llama.h     ./examples/talk-llama/llama.h
-cp -rpv ../llama.cpp/llama.cpp   ./examples/talk-llama/llama.cpp
-cp -rpv ../llama.cpp/unicode.h   ./examples/talk-llama/unicode.h
-cp -rpv ../llama.cpp/unicode.cpp ./examples/talk-llama/unicode.cpp
+cp -rpv ../llama.cpp/llama.h          ./examples/talk-llama/llama.h
+cp -rpv ../llama.cpp/llama.cpp        ./examples/talk-llama/llama.cpp
+cp -rpv ../llama.cpp/unicode.h        ./examples/talk-llama/unicode.h
+cp -rpv ../llama.cpp/unicode.cpp      ./examples/talk-llama/unicode.cpp
+cp -rpv ../llama.cpp/unicode-data.cpp ./examples/talk-llama/unicode-data.cpp
