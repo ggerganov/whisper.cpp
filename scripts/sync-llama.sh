f66187752a026011e4fccae315c266ca21a1797e
Georgi Gerganov <ggerganov@gmail.com>
2024-06-24 10:25:38 +0300

    scripts : update sync [no ci]

--- scripts/sync-llama.sh
+++ scripts/sync-llama.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cp -rpv ../llama.cpp/llama.h          ./examples/talk-llama/llama.h
-cp -rpv ../llama.cpp/llama.cpp        ./examples/talk-llama/llama.cpp
-cp -rpv ../llama.cpp/unicode.h        ./examples/talk-llama/unicode.h
-cp -rpv ../llama.cpp/unicode.cpp      ./examples/talk-llama/unicode.cpp
-cp -rpv ../llama.cpp/unicode-data.h   ./examples/talk-llama/unicode-data.h
-cp -rpv ../llama.cpp/unicode-data.cpp ./examples/talk-llama/unicode-data.cpp
+cp -rpv ../llama.cpp/include/llama.h ./examples/talk-llama/llama.h
+
+cp -rpv ../llama.cpp/src/llama.cpp        ./examples/talk-llama/llama.cpp
+cp -rpv ../llama.cpp/src/unicode.h        ./examples/talk-llama/unicode.h
+cp -rpv ../llama.cpp/src/unicode.cpp      ./examples/talk-llama/unicode.cpp
+cp -rpv ../llama.cpp/src/unicode-data.h   ./examples/talk-llama/unicode-data.h
+cp -rpv ../llama.cpp/src/unicode-data.cpp ./examples/talk-llama/unicode-data.cpp
