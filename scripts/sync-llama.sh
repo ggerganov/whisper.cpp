da43d55867826eff4d2d2d2ff2b30a97270a4a5d
Georgi Gerganov <ggerganov@gmail.com>
2024-08-08 14:16:50 +0300

    talk-llama : sync llama.cpp

--- scripts/sync-llama.sh
+++ scripts/sync-llama.sh
@@ -2,7 +2,8 @@
 
 cp -rpv ../llama.cpp/include/llama.h ./examples/talk-llama/llama.h
 
-cp -rpv ../llama.cpp/src/llama.cpp        ./examples/talk-llama/llama.cpp
+cp -rpv ../llama.cpp/src/llama*.cpp       ./examples/talk-llama/
+cp -rpv ../llama.cpp/src/llama*.h         ./examples/talk-llama/
 cp -rpv ../llama.cpp/src/unicode.h        ./examples/talk-llama/unicode.h
 cp -rpv ../llama.cpp/src/unicode.cpp      ./examples/talk-llama/unicode.cpp
 cp -rpv ../llama.cpp/src/unicode-data.h   ./examples/talk-llama/unicode-data.h
