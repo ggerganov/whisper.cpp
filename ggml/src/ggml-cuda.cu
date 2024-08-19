bd9875952c149e542e720c831857f015970723c7
Radoslav Gerganov <rgerganov@gmail.com>
2024-08-19 10:09:33 +0300

    yolo : add backend support (ggml/924)
    * yolo : add backend support
    
    * metal : add sub and sqrt kernels
    
    ---------
    
    Co-authored-by: Georgi Gerganov <ggerganov@gmail.com>

--- ggml/src/ggml-cuda.cu
+++ ggml/src/ggml-cuda.cu
@@ -2181,6 +2181,9 @@ static bool ggml_cuda_compute_forward(ggml_backend_cuda_context & ctx, struct gg
         case GGML_OP_ADD:
             ggml_cuda_op_add(ctx, dst);
             break;
+        case GGML_OP_SUB:
+            ggml_cuda_op_sub(ctx, dst);
+            break;
         case GGML_OP_ACC:
             ggml_cuda_op_acc(ctx, dst);
             break;
@@ -2859,6 +2862,7 @@ GGML_CALL static bool ggml_backend_cuda_supports_op(ggml_backend_t backend, cons
         case GGML_OP_TRANSPOSE:
         case GGML_OP_NORM:
         case GGML_OP_ADD:
+        case GGML_OP_SUB:
         case GGML_OP_MUL:
         case GGML_OP_DIV:
         case GGML_OP_RMS_NORM:
