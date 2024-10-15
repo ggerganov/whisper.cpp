ecf0c89272d3cea6a2c6843c98594a6afe15ac08
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2024-10-16 02:43:49 +0900

    Add test for build

--- bindings/ruby/tests/test_whisper.rb
+++ bindings/ruby/tests/test_whisper.rb
@@ -6,6 +6,9 @@ $:.unshift(EXTDIR)
 
 require 'whisper'
 require 'test/unit'
+require 'tempfile'
+require 'tmpdir'
+require 'shellwords'
 
 class TestWhisper < Test::Unit::TestCase
   def setup
@@ -128,4 +131,24 @@ class TestWhisper < Test::Unit::TestCase
     }
   end
 
+  def test_build
+    Tempfile.create do |file|
+      assert system("gem", "build", "whispercpp.gemspec", "--output", file.to_path.shellescape, exception: true)
+      assert_path_exist file.to_path
+    end
+  end
+
+  sub_test_case "Building binary on installation" do
+    def setup
+      system "rake", "build", exception: true
+    end
+
+    def test_install
+      filename = `rake -Tbuild`.match(/(whispercpp-(?:.+)\.gem)/)[1]
+      Dir.mktmpdir do |dir|
+        system "gem", "install", "--install-dir", dir.shellescape, "pkg/#{filename.shellescape}", exception: true
+        assert_path_exist File.join(dir, "gems/whispercpp-1.3.0/lib/whisper.bundle")
+      end
+    end
+  end
 end
