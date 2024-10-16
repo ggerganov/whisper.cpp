d3f7137cc9befa6d74dc4085de2b664b97b7c8bb
KITAITI Makoto <KitaitiMakoto@gmail.com>
2024-10-17 00:44:04 +0900

    ruby : fix bindings (#2484)
    * Improve Rakefile
    
    * Remove intermediate files
    
    * Remove unnecessary manipulations from extconf.rb
    
    * Add README and LINCENSE to source files
    
    * Manage ext source files using YAML file
    
    * Use extsources.yaml to include files into gem package file
    
    * Add git-managed source files to build dependency
    
    * Add test task
    
    * Download model for test if not exists
    
    * Add test for build
    
    * Ignore gem package directory
    
    * Enable GitHub action for Ruby binding
    
    * Fix model name
    
    * Build lib file for test
    
    * Use extension for each platform
    
    * Use extension for each platform on testing
    
    * Move built lib file rather than copy
    
    * Add intermediate files to clean targets

--- bindings/ruby/ext/extconf.rb
+++ bindings/ruby/ext/extconf.rb
@@ -1,21 +1,4 @@
 require 'mkmf'
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','whisper.cpp')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','whisper.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml.c')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-impl.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-aarch64.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-aarch64.c')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-alloc.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-alloc.c')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-backend-impl.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-backend.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-backend.cpp')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-common.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-quants.h')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','ggml-quants.c')} .")
-system("cp #{File.join(File.dirname(__FILE__),'..','..','..','examples','dr_wav.h')} .")
-
 
 # need to use c++ compiler flags
 $CXXFLAGS << ' -std=c++11'
--- bindings/ruby/tests/test_whisper.rb
+++ bindings/ruby/tests/test_whisper.rb
@@ -1,11 +1,10 @@
 TOPDIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-EXTDIR = File.join(TOPDIR, 'ext')
-#$LIBDIR = File.join(TOPDIR, 'lib')
-#$:.unshift(LIBDIR)
-$:.unshift(EXTDIR)
 
 require 'whisper'
 require 'test/unit'
+require 'tempfile'
+require 'tmpdir'
+require 'shellwords'
 
 class TestWhisper < Test::Unit::TestCase
   def setup
@@ -128,4 +127,25 @@ class TestWhisper < Test::Unit::TestCase
     }
   end
 
+  def test_build
+    Tempfile.create do |file|
+      assert system("gem", "build", "whispercpp.gemspec", "--output", file.to_path.shellescape, exception: true)
+      assert_path_exist file.to_path
+    end
+  end
+
+  sub_test_case "Building binary on installation" do
+    def setup
+      system "rake", "build", exception: true
+    end
+
+    def test_install
+      filename = `rake -Tbuild`.match(/(whispercpp-(?:.+)\.gem)/)[1]
+      basename = "whisper.#{RbConfig::CONFIG["DLEXT"]}"
+      Dir.mktmpdir do |dir|
+        system "gem", "install", "--install-dir", dir.shellescape, "pkg/#{filename.shellescape}", exception: true
+        assert_path_exist File.join(dir, "gems/whispercpp-1.3.0/lib", basename)
+      end
+    end
+  end
 end
