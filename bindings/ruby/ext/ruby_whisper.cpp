1d2d772bc5ca3d0f52a1be11409c4f0fa5614f07
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2024-10-18 03:09:30 +0900

    Don't use static for thread-safety

--- bindings/ruby/ext/ruby_whisper.cpp
+++ bindings/ruby/ext/ruby_whisper.cpp
@@ -206,28 +206,24 @@ static VALUE ruby_whisper_transcribe(int argc, VALUE *argv, VALUE self) {
     };
     rwp->params.encoder_begin_callback_user_data = &is_aborted;
   }
-  {
-    // This cannot be used later because it is not incremented when new_segment_callback is not given.
-    static int n_segments = 0;
 
-    rwp->params.new_segment_callback = [](struct whisper_context * ctx, struct whisper_state * state, int n_new, void * user_data) {
-      VALUE callback = *(VALUE *)user_data;
-      if (NIL_P(callback)){
-        return;
-      }
+  rwp->params.new_segment_callback = [](struct whisper_context * ctx, struct whisper_state * state, int n_new, void * user_data) {
+    VALUE callback = *(VALUE *)user_data;
+    if (NIL_P(callback)){
+      return;
+    }
 
-      for (int i = 0; i < n_new; i++) {
-        const int i_segment = n_segments + i;
-        const char * text = whisper_full_get_segment_text_from_state(state, i_segment);
-        // Multiplying 10 shouldn't cause overflow because to_timestamp() in whisper.cpp does it
-        const int64_t t0 = whisper_full_get_segment_t0_from_state(state, i_segment) * 10;
-        const int64_t t1 = whisper_full_get_segment_t1_from_state(state, i_segment) * 10;
-        rb_funcall(callback, rb_intern("call"), 4, rb_str_new2(text), INT2NUM(t0), INT2NUM(t1), INT2FIX(i_segment));
-      }
-      n_segments += n_new;
-    };
-    rwp->params.new_segment_callback_user_data = &rwp->new_segment_callback;
-  }
+    int n_segments = whisper_full_n_segments_from_state(state);
+    for (int i = n_new; i > 0; --i) {
+      const int i_segment = n_segments - i;
+      const char * text = whisper_full_get_segment_text_from_state(state, i_segment);
+      // Multiplying 10 shouldn't cause overflow because to_timestamp() in whisper.cpp does it
+      const int64_t t0 = whisper_full_get_segment_t0_from_state(state, i_segment) * 10;
+      const int64_t t1 = whisper_full_get_segment_t1_from_state(state, i_segment) * 10;
+      rb_funcall(callback, rb_intern("call"), 4, rb_str_new2(text), INT2NUM(t0), INT2NUM(t1), INT2FIX(i_segment));
+    }
+  };
+  rwp->params.new_segment_callback_user_data = &rwp->new_segment_callback;
 
   if (whisper_full_parallel(rw->context, rwp->params, pcmf32.data(), pcmf32.size(), 1) != 0) {
     fprintf(stderr, "failed to process audio\n");
