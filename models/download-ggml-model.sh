9c77e8f3ff900e4a1c696ee0925dd9657c70700d
Michael Rienstra <mrienstra@gmail.com>
2024-11-25 12:19:27 -0800

    Add `q8_0` models to `download-ggml-model.sh`

--- models/download-ggml-model.sh
+++ models/download-ggml-model.sh
@@ -29,26 +29,32 @@ models="tiny
 tiny.en
 tiny-q5_1
 tiny.en-q5_1
+tiny-q8_0
 base
 base.en
 base-q5_1
 base.en-q5_1
+base-q8_0
 small
 small.en
 small.en-tdrz
 small-q5_1
 small.en-q5_1
+small-q8_0
 medium
 medium.en
 medium-q5_0
 medium.en-q5_0
+medium-q8_0
 large-v1
 large-v2
 large-v2-q5_0
+large-v2-8_0
 large-v3
 large-v3-q5_0
 large-v3-turbo
-large-v3-turbo-q5_0"
+large-v3-turbo-q5_0
+large-v3-turbo-q8_0"
 
 # list available models
 list_models() {
