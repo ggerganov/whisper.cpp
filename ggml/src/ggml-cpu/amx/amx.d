3daeacad24f690376a6b7f62a646f062e69433ae
Diego Devesa <slarengh@gmail.com>
2024-12-03 20:22:12 +0200

    ggml : move AMX to the CPU backend (llama/10570)
    ggml : automatic selection of best CPU backend (llama/10606)

---
+++ ggml/src/ggml-cpu/amx/amx.d
@@ -0,0 +1,6 @@
+ggml/src/ggml-cpu/amx/amx.o: ggml/src/ggml-cpu/amx/amx.cpp \
+  ggml/src/ggml-cpu/amx/amx.h ggml/include/ggml-backend.h \
+  ggml/include/ggml.h ggml/include/ggml-alloc.h \
+  ggml/src/ggml-cpu/ggml-cpu-impl.h ggml/src/ggml-impl.h \
+  ggml/src/ggml-cpu/amx/common.h ggml/src/ggml-cpu/amx/mmq.h \
+  ggml/src/ggml-backend-impl.h ggml/include/ggml-cpu.h
