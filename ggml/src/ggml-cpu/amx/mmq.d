22616f00f963b4e210cc925e7289e8f876f2fbf3
Georgi Gerganov <ggerganov@gmail.com>
2024-12-03 20:29:32 +0200

    files : remove make artifacts

--- ggml/src/ggml-cpu/amx/mmq.d
+++
@@ -1,7 +0,0 @@
-ggml/src/ggml-cpu/amx/mmq.o: ggml/src/ggml-cpu/amx/mmq.cpp \
-  ggml/src/ggml-cpu/amx/amx.h ggml/include/ggml-backend.h \
-  ggml/include/ggml.h ggml/include/ggml-alloc.h \
-  ggml/src/ggml-cpu/ggml-cpu-impl.h ggml/src/ggml-impl.h \
-  ggml/src/ggml-cpu/amx/mmq.h ggml/src/ggml-cpu/amx/common.h \
-  ggml/src/ggml-cpu/ggml-cpu-quants.h ggml/src/ggml-common.h \
-  ggml/src/ggml-quants.h
