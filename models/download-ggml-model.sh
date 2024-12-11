ggerganov/whisper.cpp

commit a9d06ce1518c5abd8c313254522f5c160b26c448

models : add `q8_0` models to `download-ggml-model.sh` (#2589)

models/download-ggml-model.sh
@@ -29,26 +29,32 @@ models="tiny
 tiny.en
 tiny-q5_1
 tiny.en-q5_1
+tiny-q8_0
 base
 base.en
 base-q5_1
 base.en-q5_1
+base-q8_0
 small
 small.en
 small.en-tdrz
 small-q5_1
 small.en-q5_1
+small-q8_0
 medium
 medium.en
 medium-q5_0
 medium.en-q5_0
+medium-q8_0
 large-v1
 large-v2
 large-v2-q5_0
+large-v2-8_0
 large-v3
 large-v3-q5_0
 large-v3-turbo
-large-v3-turbo-q5_0"
+large-v3-turbo-q5_0
+large-v3-turbo-q8_0"
 
 # list available models
 list_models() {

commit 49c33aa40d1dad2b65e1fdb864a9a30d21c5c3d8

Fix typo in download-ggml-model.sh
Introduced in #2589

models/download-ggml-model.sh
@@ -49,7 +49,7 @@ medium-q8_0
 large-v1
 large-v2
 large-v2-q5_0
-large-v2-8_0
+large-v2-q8_0
 large-v3
 large-v3-q5_0
 large-v3-turbo