afdac93d5e2c187530573bca5714cb39ddb7b764
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-03-20 06:10:32 +0100

    Revert "examples : fix tautological-compare warning in stb_vorbis.c [no ci]"
    This reverts commit 8e3c47d96141c7675c985562ebdc705e839e338a.
    
    We should not make this change here and instead when the upstream PR is
    merged we can sync with it.

--- examples/stb_vorbis.c
+++ examples/stb_vorbis.c
@@ -1401,7 +1401,7 @@ static int set_file_offset(stb_vorbis *f, unsigned int loc)
    #endif
    f->eof = 0;
    if (USE_MEMORY(f)) {
-      if (loc >= f->stream_len) {
+      if (f->stream_start + loc >= f->stream_end || f->stream_start + loc < f->stream_start) {
          f->stream = f->stream_end;
          f->eof = 1;
          return 0;
