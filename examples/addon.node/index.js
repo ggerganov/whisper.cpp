ffd5191f46968497c3afad7c2b69018f3ee2bc54
linxiaodong <calm.lin@wukongsch.com>
2025-03-25 10:48:14 +0800

    feat: progress supported

--- examples/addon.node/index.js
+++ examples/addon.node/index.js
@@ -19,6 +19,9 @@ const whisperParams = {
   no_timestamps: false,
   audio_ctx: 0,
   max_len: 0,
+  progress_callback: (progress) => {
+      console.log(`progress: ${progress}%`);
+    }
 };
 
 const arguments = process.argv.slice(2);
