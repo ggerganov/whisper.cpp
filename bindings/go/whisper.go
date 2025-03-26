ab09a286b5cab23508a467a11ff01f5856372e87
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-03-26 15:33:32 +0100

    bindings-go : update Makefile to use cmake
    This commit updates the Makefile to use cmake instead of make to build
    whisper.cpp.
    
    The motivation for this change is that currently the make recipe test
    will fail with the following error:
    ```console
    $ make test
    Mkdir build
    Mkdir models
    Build whisper
    make[1]: Entering directory '/home/danbev/work/ai/whisper-work'
    make[1]: *** No rule to make target 'libwhisper.a'.  Stop.
    make[1]: Leaving directory '/home/danbev/work/ai/whisper-work'
    make: *** [Makefile:33: whisper] Error 2
    ```

--- bindings/go/whisper.go
+++ bindings/go/whisper.go
@@ -9,7 +9,7 @@ import (
 // CGO
 
 /*
-#cgo LDFLAGS: -lwhisper -lm -lstdc++ -fopenmp
+#cgo LDFLAGS: -lwhisper -lggml -lggml-base -lggml-cpu  -lm -lstdc++ -fopenmp
 #cgo darwin LDFLAGS: -framework Accelerate -framework Metal -framework Foundation -framework CoreGraphics
 #include <whisper.h>
 #include <stdlib.h>
