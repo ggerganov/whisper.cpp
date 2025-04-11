b227edb33ea703c87884d827794a1ce2474dc661
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-04-11 23:17:20 +0900

    Search error on CI

--- bindings/ruby/tests/helper.rb
+++ bindings/ruby/tests/helper.rb
@@ -2,6 +2,11 @@ require "test/unit"
 require "whisper"
 require_relative "jfk_reader/jfk_reader"
 
+puts "ldd lib/whisper.so"
+puts `ldd lib/whisper.so`
+puts "nm lib/whisper.so"
+puts `nm lib/whisper.so`
+
 class TestBase < Test::Unit::TestCase
   AUDIO = File.join(__dir__, "..", "..", "..", "samples", "jfk.wav")
 
