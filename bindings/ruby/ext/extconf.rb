718f62f0dd25155e995d6911e58c3b9c7e6d03e0
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-04-11 10:10:11 +0900

    Enable PIC

--- bindings/ruby/ext/extconf.rb
+++ bindings/ruby/ext/extconf.rb
@@ -19,7 +19,7 @@ create_makefile "whisper" do |conf|
     $(TARGET_SO): #{libs.join(" ")}
     #{libs.join(" ")}: cmake-targets
     cmake-targets:
-    #{"\t"}#{cmake} -S sources -B build -D BUILD_SHARED_LIBS=OFF -D CMAKE_ARCHIVE_OUTPUT_DIRECTORY=#{__dir__}
+    #{"\t"}#{cmake} -S sources -B build -D BUILD_SHARED_LIBS=OFF -D CMAKE_ARCHIVE_OUTPUT_DIRECTORY=#{__dir__} -D CMAKE_POSITION_INDEPENDENT_CODE=ON
     #{"\t"}#{cmake} --build build --config Release --target common whisper
   EOF
 end
