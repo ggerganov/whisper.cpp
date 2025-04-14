46bfa146df7c748562ad282c9f3d8b56013f1e97
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-04-14 15:22:28 +0900

    Check dependency of whisper.cpp accurately

--- bindings/ruby/ext/extconf.rb
+++ bindings/ruby/ext/extconf.rb
@@ -1,4 +1,5 @@
 require "mkmf"
+require "tsort"
 
 # TODO: options such as CoreML
 
@@ -8,20 +9,53 @@ have_library("gomp") rescue nil
 
 prefix = File.join("build", "whisper.cpp.dot")
 system cmake, "-S", "sources", "-B", "build", "--graphviz", prefix, "-D", "BUILD_SHARED_LIBS=OFF", exception: true
-libs = Dir["#{prefix}.*"].collect {|file|
-  "lib" + file.sub("#{prefix}.", "") + ".a"
-}
 
+static_lib_shape = nil
+nodes = {}
+depends = {}
+class << depends
+  include TSort
+  alias tsort_each_node each_key
+  def tsort_each_child(node, &block)
+    fetch(node, []).each(&block)
+  end
+end
+File.open(File.join("build", "whisper.cpp.dot")).each_line do |line|
+  case line
+  when /\[\s*label\s*=\s*"Static Library"\s*,\s*shape\s*=\s*(?<shape>\w+)\s*\]/
+    static_lib_shape = $~[:shape]
+  when /\A\s*"(?<node>\w+)"\s*\[\s*label\s*=\s*"(?<label>\S+)"\s*,\s*shape\s*=\s*(?<shape>\w+)\s*\]\s*;\s*\z/
+    node = $~[:node]
+    label = $~[:label]
+    shape = $~[:shape]
+    nodes[node] = [label, shape]
+  when /\A\s*"(?<depender>\w+)"\s*->\s*"(?<dependee>\w+)"/
+    depender = $~[:depender]
+    dependee = $~[:dependee]
+    depends[depender] ||= []
+    depends[depender] << dependee
+  end
+end
+libs = depends.tsort.filter_map {|node|
+  label, shape = nodes[node]
+  shape == static_lib_shape ? label : nil
+}.collect {|lib| "lib#{lib}.a"}
+ .reverse
+ .join(" ")
+
+$CFLAGS << " -std=c11 -fPIC"
+$CXXFLAGS << " -std=c++17 -O3 -DNDEBUG"
 $INCFLAGS << " -Isources/include -Isources/ggml/include -Isources/examples"
-$LOCAL_LIBS << " " << libs.join(" ")
-$cleanfiles << " build" << libs.join(" ")
+$LOCAL_LIBS << " #{libs}"
+$cleanfiles << " build #{libs}"
 
 create_makefile "whisper" do |conf|
   conf << <<~EOF
-    $(TARGET_SO): #{libs.join(" ")}
-    #{libs.join(" ")}: cmake-targets
+    $(TARGET_SO): #{libs}
+    #{libs}: cmake-targets
     cmake-targets:
     #{"\t"}#{cmake} -S sources -B build -D BUILD_SHARED_LIBS=OFF -D CMAKE_ARCHIVE_OUTPUT_DIRECTORY=#{__dir__} -D CMAKE_POSITION_INDEPENDENT_CODE=ON
     #{"\t"}#{cmake} --build build --config Release --target common whisper
+    #{"\t"}
   EOF
 end
